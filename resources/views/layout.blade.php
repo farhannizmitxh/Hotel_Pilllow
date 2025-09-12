4460fbbee096bf99b3d9e390bcc8f4a60913635e
Rhaditya <rhaditya203@gmail.com>
2025-09-12 09:57:51 +0700

    menambahkan logo pillo

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -61,7 +61,7 @@
                 <div class="app-brand demo">
                     <a href="index.html" class="app-brand-link">
                         <span class="app-brand-logo demo">
-                            <img src="{{ asset('assets/img/logo/images.jpeg') }}" alt="Logo Pillo" width="70" />
+                            <img src="{{ asset('assets/img/logo_pillo.png') }}" alt="Logo Pillo" width="70" />
                         </span>
                     </a>
 
