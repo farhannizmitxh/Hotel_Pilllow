2c5b099a13da1cad9100544e22cfe506585db0ca
veriarajatunggal <veriarajatunggal@gmail.com>
2025-09-12 10:15:02 +0700

    Added CRUD reservation

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Reservation;
+use App\Models\Guest;
 use Illuminate\Http\Request;
 
 class ReservationController extends Controller
@@ -12,39 +13,45 @@ class ReservationController extends Controller
      */
     public function index()
     {
-        //
+        // Variabel Guests berisikan Model Guest yang diperintahkan untuk mengambil semua data pada tabel guest
+        $guests = Guest::all();
+        $reservations = Reservation::all();
+        // $reservations = Reservation::where('guest_id', 1)->get();
+        return view('reservation.index', compact('guests', 'reservations'));
     }
-
+    
     /**
      * Show the form for creating a new resource.
-     */
+    */
     public function create()
     {
         //
     }
-
+    
     /**
      * Store a newly created resource in storage.
-     */
+    */
     public function store(Request $request)
     {
-        //
+        Reservation::create($request->all());
+        return back();
     }
-
+    
     /**
      * Display the specified resource.
-     */
+    */
     public function show(Reservation $reservation)
     {
         //
     }
-
+    
     /**
      * Show the form for editing the specified resource.
-     */
+    */
     public function edit(Reservation $reservation)
     {
-        //
+        $guests = Guest::all();
+        return view('reservation.edit', compact('reservation', 'guests'));
     }
 
     /**
@@ -52,7 +59,13 @@ class ReservationController extends Controller
      */
     public function update(Request $request, Reservation $reservation)
     {
-        //
+        $reservation->code      = $request->code;
+        $reservation->guest_id  = $request->guest_id;
+        $reservation->status    = $request->status;
+        $reservation->voucher   = $request->voucher;
+        $reservation->update();
+
+        return redirect('reservation');
     }
 
     /**
@@ -60,6 +73,8 @@ class ReservationController extends Controller
      */
     public function destroy(Reservation $reservation)
     {
-        //
+        $reservation->delete();
+
+        return back();
     }
 }
